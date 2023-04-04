1d6a89d69daab7815c158f23a3c26de7d205c65f
snoopy1224 <misty12024@gmail.com>
2023-04-04 16:55:53 +0900

    리액트를 다루는 기술

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import IterationSample from "./chapter6/IterationSample";
+
+function App() {
+  return (
+    <div className="App">
+      <IterationSample />
+    </div>
+  );
+}
+
+export default App;
