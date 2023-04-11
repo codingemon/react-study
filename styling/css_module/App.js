a8d2eb10ac43d4a09c96bec94226c25377fd167b
snoopy1224 <misty12024@gmail.com>
2023-04-11 19:54:56 +0900

    css_module

---
+++ styling/css_module/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CSSModule from "./CSSModule";
+
+function App() {
+  return (
+    <div>
+      <CSSModule />
+    </div>
+  );
+}
+
+export default App;
---
+++ styling/css_module/CSSModule.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./CSSModule.module.css";
+
+const CSSModule = () => {
+  return (
+    <div>
+      <div className={`{styles.wrapper} ${styles.inverted}`}>
+        안녕하세요, 저는 <span className="something">CSS Module!</span>
+      </div>
+    </div>
+  );
+};
+
+export default CSSModule;
