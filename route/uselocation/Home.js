8c2e9ee977e7f4178764d83a4e30df8e24bbd62c
snoopy1224 <misty12024@gmail.com>
2023-04-19 15:07:51 +0900

    useloction v6 버전

---
+++ route/uselocation/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>홈</h1>
+      <p>홈페이지</p>
+      <ul>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">개발자 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">홍길동 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/void">존재하지 않는 프로필</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
