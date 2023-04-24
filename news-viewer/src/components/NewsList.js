57b8a6d0e027c93d3112c10c9fc8f627f3eba93d
snoopy1224 <misty12024@gmail.com>
2023-04-24 21:34:53 +0900

    react 카테고리

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticels] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -25,8 +25,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=a112ac2d9a114393b58b9430fb41a2ec',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a112ac2d9a114393b58b9430fb41a2ec`,
         );
         setArticels(response.data.articles);
       } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   // 대기 중일 때
   if (loading) {
