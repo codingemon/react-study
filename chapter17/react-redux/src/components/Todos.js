3decce5cdcb87c9011b1c192856d493ff612d68c
snoopy1224 <misty12024@gmail.com>
2023-05-02 15:08:51 +0900

    리액트 리덕스

--- chapter17/react-redux/src/components/Todos.js
+++ chapter17/react-redux/src/components/Todos.js
@@ -4,9 +4,16 @@ import React from 'react';
 const TodoItem = ({ todo, onToggle, onRemove }) => {
   return (
     <div>
-      <input type="checkbox" />
-      <span>예제 텍스트</span>
-      <button>삭제</button>
+      <input
+        type="checkbox"
+        onClick={() => onToggle(todo.id)}
+        checked={todo.done}
+        readOnly={true}
+      />
+      <span style={{ textDecoration: todo.done ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={() => onRemove(todo.id)}>삭제</button>
     </div>
   );
 };
@@ -23,19 +30,25 @@ const Todos = ({
     // preventDefault() 메서드는 어떤 이벤트를 명시적으로 처리하지 않은 경우,
     // 해당 이벤트에 대한 사용자 에이전트의 기본 동작을 실행하지 않도록 지정합니다.
     e.preventDefault();
+    onInsert(input);
+    onChangeInput(''); // 등록 후 인풋 초기화
   };
+  const onChange = (e) => onChangeInput(e.target.value);
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input />
+        <input value={input} onChange={onChange} />
         <button type="sumbit">등록</button>
       </form>
       <div>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todos.map((todo) => (
+          <TodoItem
+            todo={todo}
+            key={todo.id}
+            onToggle={onToggle}
+            onRemove={onRemove}
+          />
+        ))}
       </div>
     </div>
   );
--- chapter17/react-redux/src/containers/CounterContainer.js
+++ chapter17/react-redux/src/containers/CounterContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
   return (
@@ -16,10 +17,23 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   // 임시 함수
   increase: () => {
-    console.log('increase');
+    dispatch(increase()); // 액션객체를 가져와서 디스패치해 주었다.
   },
   decrease: () => {
-    console.log('decrease');
+    dispatch(decrease()); // 액션객체를 가져와서 디스패치해 주었다.
   },
 });
+
 export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+//취향에 따라 밑처럼 작성해줘도 괜찮다고 하는데 나는 작동되지 않았다.
+
+// export default connect(
+//     state => ({
+//       number: state.counter.number,
+//     }),
+//     {
+//       increase,
+//       decrease,
+//     },
+//   )(CounterCotainer);
