5004c9b01ba1501089642f541b5d0027c0e06012
snoopy1224 <misty12024@gmail.com>
2023-05-03 23:09:02 +0900

    커밋전에 체크

--- chapter17/react-redux/src/modules/todos.js
+++ chapter17/react-redux/src/modules/todos.js
@@ -1,10 +1,10 @@
 // todos 모듈 만들기
 
 // 액션 타입 정의하기
-const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
-const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
-const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
-const REMOVE = 'todos/REMOVE'; // todo를 제거함
+const CHANGE_INPUT = "todos/CHANGE_INPUT"; // 인풋 값을 변경함
+const INSERT = "todos/INSERT"; // 새로운 todo를 등록함
+const TOGGLE = "todos/TOGGLE"; // todo를 체크/체크 해제함
+const REMOVE = "todos/REMOVE"; // todo를 제거함
 
 // 액션 생성 함수 만들기
 // 액션 생성 함수에서 파라미터가 필요하다.
@@ -14,7 +14,7 @@ export const changeInput = (input) => ({
   input,
 });
 
-let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+let id = 3; // insert가 호출될 때마다 1씩 더해집니다..
 export const insert = (text) => ({
   type: INSERT,
   todo: {
@@ -37,16 +37,16 @@ export const remove = (id) => ({
 // 초기 상태 및 리듀서 함수 만들기
 
 const initialState = {
-  input: '',
+  input: "",
   todos: [
     {
       id: 1,
-      text: '리덕스 초기 배우기',
+      text: "리덕스 초기 배우기",
       done: true,
     },
     {
       id: 2,
-      text: '리액트와 리덕스 사용하기',
+      text: "리액트와 리덕스 사용하기",
       done: false,
     },
   ],
@@ -58,7 +58,7 @@ function todos(state = initialState, action) {
       return {
         ...state,
         todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo,
+          todo.id === action.id ? { ...todo, done: !todo.done } : todo
         ),
       };
     case REMOVE:
