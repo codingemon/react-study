27cc46b98a5baf16efa9e7c628eaeea72347b0f1
snoopy1224 <misty12024@gmail.com>
2023-05-04 13:23:09 +0900

    리덕스 immer 사용해보기

--- chapter17/react-redux/src/modules/counter.js
+++ chapter17/react-redux/src/modules/counter.js
@@ -1,11 +1,19 @@
+// counter 모듈에 적용하기
+import { createAction, handleActions } from 'redux-actions';
 // 1. 액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
+//새로 작성
+// createAction을 사용하면 매번 객체를 직접 만들어 줄 필요 없이
+// 간단하게 액션 생성 함수를 선언
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
 // 2. 액션 생성 함수 만들기
 // export를 사용해서 추후에 이 함수를 다른 파일에서 불러와 사용가능하다.
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
 
 // 3. 초기 상태 및 리듀서 함수 만들기
 
@@ -13,20 +21,31 @@ const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+// 이번에는 리듀서 함수도 더 간단하게 가독성을 높게 작성.
+// handleActions라는 함수를 사용
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 // export default를 사용하여 함수를 내보내 줬다.
 // export는 여러 개를 내보낼 수 있지만, export default는 단 한개만 내보낸다.
--- chapter17/react-redux/src/modules/todos.js
+++ chapter17/react-redux/src/modules/todos.js
@@ -1,74 +1,117 @@
 // todos 모듈 만들기
 
+import { createAction, handleAction } from 'redux-actions';
+
 // 액션 타입 정의하기
-const CHANGE_INPUT = "todos/CHANGE_INPUT"; // 인풋 값을 변경함
-const INSERT = "todos/INSERT"; // 새로운 todo를 등록함
-const TOGGLE = "todos/TOGGLE"; // todo를 체크/체크 해제함
-const REMOVE = "todos/REMOVE"; // todo를 제거함
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록함
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제함
+const REMOVE = 'todos/REMOVE'; // todo를 제거함
 
-// 액션 생성 함수 만들기
-// 액션 생성 함수에서 파라미터가 필요하다.
-// 전달 받은 파라미터는 액션 객체 안에 추가 필드로 들어간다.
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-});
+// 17.6.1.2 todos 모듈에 적용하기
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
 
-let id = 3; // insert가 호출될 때마다 1씩 더해집니다..
-export const insert = (text) => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
-});
+let id = 3; //insert 호출될 때마다 1씩 더해집니다.
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
 
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
+// // 액션 생성 함수 만들기
+// // 액션 생성 함수에서 파라미터가 필요하다.
+// // 전달 받은 파라미터는 액션 객체 안에 추가 필드로 들어간다.
+// export const changeInput = (input) => ({
+//   type: CHANGE_INPUT,
+//   input,
+// });
+
+// let id = 3; // insert가 호출될 때마다 1씩 더해집니다.
+// export const insert = (text) => ({
+//   type: INSERT,
+//   todo: {
+//     id: id++,
+//     text,
+//     done: false,
+//   },
+// });
+
+// export const toggle = (id) => ({
+//   type: TOGGLE,
+//   id,
+// });
+
+// export const remove = (id) => ({
+//   type: REMOVE,
+//   id,
+// });
 
 // 초기 상태 및 리듀서 함수 만들기
-
 const initialState = {
-  input: "",
+  input: '',
   todos: [
     {
       id: 1,
-      text: "리덕스 초기 배우기",
+      text: '리덕스 초기 배우기',
       done: true,
     },
     {
       id: 2,
-      text: "리액트와 리덕스 사용하기",
+      text: '리액트와 리덕스 사용하기',
       done: false,
     },
   ],
 };
 
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
-    default:
-      return state;
-  }
-}
+const todos = handleAction(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState,
+);
+
+// immer를 사용한다고해서 모든 업데이트 함수에 immer를 적용할 필요는 없다.
+// 위에 코드도 괜찬하서 아래 코드를 사용하지 않아도 된다.
+
+// const todos = handleAction(
+//   {
+//     [CHANGE_INPUT]: (state, { payload: input }) =>
+//       produce(state, (draft) => {
+//         draft.input = input;
+//       }),
+//     [INSERT]: (state, { payload: todo }) =>
+//       produce(state, (draft) => {
+//         draft.todos.push(todo);
+//       }),
+//     [TOGGLE]: (state, { payload: id }) =>
+//       produce(state, (draft) => {
+//         const todo = draft.todos.find((todo) => todo.id === id);
+//         todo.done = !todo.done;
+//       }),
+//     [REMOVE]: (state, { payload: id }) =>
+//       produce(state, (draft) => {
+//         const index = draft.todos.findIndex((todo) => todo.id === id);
+//         draft.todos.splice(index, 1);
+//       }),
+//   },
+//   initialState,
+// );
 
 export default todos;
