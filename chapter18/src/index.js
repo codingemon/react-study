b869c3246b8ecee1516f9514e3fff576a8beab9b
snoopy1224 <misty12024@gmail.com>
2023-05-05 22:33:48 +0900

    리듀서 응용

---
+++ chapter18/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
+ServiceWorker.unregister();
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
---
+++ chapter18/src/modules/counter.js
@@ -0,0 +1,20 @@
+// 리덕스 모듈
+import { createAction, handleActions } from "redux-actions";
+
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+const initialState = 0; // 상태는 꼭 객체일 필요가 없음. 숫자도 작동한다
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
+
+export default counter;
