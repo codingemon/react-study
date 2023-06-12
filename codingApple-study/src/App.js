4d1930b0f5a4346b9a69fc9031164fff018fa94d
snoopy1224 <misty12024@gmail.com>
2023-06-12 13:57:14 +0900

    state

--- codingApple-study/src/App.js
+++ codingApple-study/src/App.js
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   let post = "도라에몽 강남점";
+  // 자료를 잠깐 보관,저장할 땐 state 써도 된다
+  // 1. import{useState} 2.useState{보관할 자료} 3.let[작명,작명]
+  // (참고) Destructuring 문법
+  // Q.state 언제써야하나? -> 변동시 자동으로 html에 반영되게 만들고 싶으면 state를 사용
+  // 자주변경될거 같은 html부분은 state로 만들어놓기
+  let [title, setTitle] = useState("도라에몽 만화 추천");
+  let [logo, setLogo] = useState("도라에몽 블로그");
 
   return (
     <div className="App">
-      {/* ==== JSX 문법 ====  */}
-      {/* class x , className */}
       <div className="black-nav">
-        {/* style 넣을 땐 style={{스타일명:'값'}}  */}
-        <h4 style={{ color: "red", fontSize: "20px" }}>블로그임</h4>
+        <h4>{logo}</h4>
       </div>
-      {/* 변수넣을 땐 {중괄호} */}
-      <h4>{post}</h4>
+      <div className="list">
+        <h4>{title}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
