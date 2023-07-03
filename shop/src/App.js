c6affb0eb2bfec5470984dbe25273a0ef1f041f5
snoopy1224 <misty12024@gmail.com>
2023-07-03 19:44:19 +0900

    쇼핑몰

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import 작명 from "./data.js";
 
 function App() {
+  let [shoes] = useState();
+
   return (
     <div className="App">
       <Navbar expand="lg" className="bg-body-tertiary">
@@ -11,7 +15,7 @@ function App() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">홈으로</Nav.Link>
-              <Nav.Link href="#goods">굿즈</Nav.Link>
+              <Nav.Link href="#cart">장바구니</Nav.Link>
               <Nav.Link href="#movie">극장판</Nav.Link>
               <NavDropdown title="메뉴" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">
@@ -26,6 +30,37 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ shop/src/data.js
@@ -0,0 +1,7 @@
+let a = 10;
+
+// export 하려면
+// export default 변수명
+// export 여러개 하려면
+// export {변수1, 변수2}
+export default a;
