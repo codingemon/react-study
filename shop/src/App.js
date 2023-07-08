03bb0c7a580a7cd68fd0f56226c34fd0ede5667b
snoopy1224 <misty12024@gmail.com>
2023-07-08 20:48:57 +0900

    쇼핑몰

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
@@ -14,6 +15,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
+              <Link to="/">홈</Link>
+              <Link to="/detail">상세페이지</Link>
               <Nav.Link href="#home">홈으로</Nav.Link>
               <Nav.Link href="#cart">장바구니</Nav.Link>
               <Nav.Link href="#movie">극장판</Nav.Link>
@@ -31,17 +34,27 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
-
-      <div className="container">
-        <div className="row">
-          {/* <Card shoes={shoes[1]} i={2}></Card>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              {" "}
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {/* <Card shoes={shoes[1]} i={2}></Card>
           <Card shoes={shoes[2]} i={3}></Card> */}
-          {shoes.map((a, i) => {
-            return <Card shoes={shoes[i]} i={i}></Card>;
-          })}
-        </div>
-      </div>
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i}></Card>;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<div>어바웃페이지임</div>} />
+      </Routes>
     </div>
   );
 }
