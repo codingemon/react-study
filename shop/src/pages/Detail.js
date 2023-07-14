57a38ed0fb914f2064b83ac0fff088b233dacb5e
snoopy1224 <misty12024@gmail.com>
2023-07-14 19:34:11 +0900

    쇼핑몰

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -12,6 +12,14 @@ function Detail(props) {
   let [alert, setAlert] = useState(true);
   let [num, setNum] = useState("");
   let [tab, setTab] = useState(0);
+  let [fade2, setFade2] = useState("");
+
+  useEffect(() => {
+    setFade2("end");
+    return () => {
+      setFade2("");
+    };
+  }, []);
 
   useEffect(() => {
     let a = setTimeout(() => {
@@ -29,69 +37,80 @@ function Detail(props) {
   }, [num]);
 
   return (
-    <div className="container">
-      {alert == true ? (
-        <div className="alert alert-warning">2초이내 구매시 할인</div>
-      ) : null}
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src="https://codingapple1.github.io/shop/shoes1.jpg"
-            width="100%"
-          />
+    <div className={"container start " + fade2}>
+      <div className="container">
+        {alert == true ? (
+          <div className="alert alert-warning">2초이내 구매시 할인</div>
+        ) : null}
+        <div className="row">
+          <div className="col-md-6">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="100%"
+            />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">{searchItem.title}</h4>
+            <p>{searchItem.content}</p>
+            <p>{searchItem.price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
         </div>
-        <div className="col-md-6">
-          <h4 className="pt-5">{searchItem.title}</h4>
-          <p>{searchItem.content}</p>
-          <p>{searchItem.price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-        </div>
+        <Nav variant="tabs" defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link
+              onClick={() => {
+                setTab(0);
+              }}
+              eventKey="link0"
+            >
+              버튼0
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              onClick={() => {
+                setTab(1);
+              }}
+              eventKey="link1"
+            >
+              버튼1
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              onClick={() => {
+                setTab(2);
+              }}
+              eventKey="link2"
+            >
+              버튼2
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent tab={tab} />
       </div>
-      <Nav variant="tabs" defaultActiveKey="link0">
-        <Nav.Item>
-          <Nav.Link
-            onClick={() => {
-              setTab(0);
-            }}
-            eventKey="link0"
-          >
-            버튼0
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            onClick={() => {
-              setTab(1);
-            }}
-            eventKey="link1"
-          >
-            버튼1
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            onClick={() => {
-              setTab(2);
-            }}
-            eventKey="link2"
-          >
-            버튼2
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <TabContent tab={tab} />
     </div>
   );
 }
 
 function TabContent({ tab }) {
-  if (tab == 0) {
-    return <div>내용0</div>;
-  } else if (tab == 1) {
-    return <div>내용1</div>;
-  } else if (tab == 2) {
-    return <div>내용2</div>;
-  }
+  let [fade, setFade] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFade("end");
+    }, 100);
+    return () => {
+      setFade("");
+    };
+  }, [tab]);
+
+  return (
+    <div className={"start " + fade}>
+      {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}
+    </div>
+  );
 }
 
 export default Detail;
