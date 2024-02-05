f3d2ace281c052a10552104fcc04740524c02c0b
snoopy1224 <misty12024@gmail.com>
2024-02-05 11:35:18 +0900

    2. 삼항 연산자

--- 한입크기리액트스터디/js응용/2.js
+++ 한입크기리액트스터디/js응용/2.js
@@ -1,26 +1,26 @@
 // 2. 삼항 연산자
-let a = 3;
-a >= 0 ? console.log("양수") : console.log("음수"); // 양수
+// let a = 3;
+// a >= 0 ? console.log("양수") : console.log("음수"); // 양수
 
-let a = [];
-a.length === 0 ? console.log("빈배열") : console.log("안 빈 배열"); // 빈배열
+// let a = [];
+// a.length === 0 ? console.log("빈배열") : console.log("안 빈 배열"); // 빈배열
 
-// 값이 참일때와 거짓일 때
-let a = [1, 2];
+// // 값이 참일때와 거짓일 때
+// let a = [1, 2];
 
-const arryStatus = a.length === 0 ? "빈배열" : "안 빈 배열";
-console.log(arryStatus);
+// const arryStatus = a.length === 0 ? "빈배열" : "안 빈 배열";
+// console.log(arryStatus);
 
-// truthy & falsy
-let a;
+// // truthy & falsy
+// let a;
 
-const result = a ? true : false;
-console.log(result); // false
+// const result = a ? true : false;
+// console.log(result); // false
 
-let a = [];
+// let a = [];
 
-const result = a ? true : false;
-console.log(result); //true
+// const result = a ? true : false;
+// console.log(result); //true
 
 // 학점 계산 프로그램
 // 중첩 삼항 연산자
