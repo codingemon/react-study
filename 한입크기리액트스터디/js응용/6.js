25662d76d6c829ceaa19ee95e03d2ad35e9d4901
snoopy1224 <misty12024@gmail.com>
2024-02-06 23:20:20 +0900

    Spread 연산자

---
+++ 한입크기리액트스터디/js응용/6.js
@@ -0,0 +1,53 @@
+// 6. spread 연산자
+// const cookie = {
+//     base: "cookie",
+//     madeIn: "korea"
+//   };
+
+//   const chocochipCookie = {
+//     base: "cookie",
+//     madeIn: "korean",
+//     toping: "cocochip"
+//   };
+
+//   const blueberryCookie = {
+//     base: "cookie",
+//     madeIn: "korea",
+//     toping: "blueberry"
+//   };
+
+//   const strawberryCookie = {
+//     base: "cookie",
+//     madeIn: "korea",
+//     toping: "strawberry"
+//   };
+
+const cookie = {
+  base: "cookie",
+  madeIn: "korea",
+};
+
+const chocochipCookie = {
+  ...cookie,
+  toping: "cocochip",
+};
+
+const blueberryCookie = {
+  ...cookie,
+  toping: "blueberry",
+};
+
+const strawberryCookie = {
+  ...cookie,
+  toping: "strawberry",
+};
+
+console.log(chocochipCookie);
+
+// 배열에서 사용
+const noTopingCookies = ["촉촉한쿠기", "안촉촉한쿠키"];
+const topingCookies = ["바나나", "초코", "아무거나"];
+
+const allCookies = [...noTopingCookies, ...topingCookies];
+
+console.log(allCookies);
