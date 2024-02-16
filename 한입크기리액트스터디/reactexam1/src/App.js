e0fdf86af6525c54cd7464d8a432d1c9634d74fc
snoopy1224 <misty12024@gmail.com>
2024-02-16 22:58:56 +0900

    1. Create React App

---
+++ 한입크기리액트스터디/reactexam1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  let name = "피카츄";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>가보자고 리액트 {name}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
