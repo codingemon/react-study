b11553f09aa22aa5a9db828def1a250c19df6357
snoopy1224 <misty12024@gmail.com>
2024-02-17 23:09:48 +0900

    4. State

--- 한입크기리액트스터디/reactexam1/src/App.js
+++ 한입크기리액트스터디/reactexam1/src/App.js
@@ -1,30 +1,13 @@
 // import "./App.css";
 import React from "react";
 import MyHeader from "./MyHeader";
+import Counter from "./Counter";
 
 function App() {
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    },
-  };
-
-  const number = 200340;
-
   return (
-    <div style={style.App}>
+    <div>
       <MyHeader />
-      <h2 style={style.h2}>가보자고 리액트 </h2>
-      <b id="bold_text" style={style.bold_text}>
-        React
-        {number}는 : {number % 2 === 0 ? "짝수" : "홀수"}
-      </b>
+      <Counter />
     </div>
   );
 }
