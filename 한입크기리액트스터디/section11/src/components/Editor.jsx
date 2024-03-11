c2a30f06f4a3032b7076c1948c7f73a7511a05b7
snoopy1224 <misty12024@gmail.com>
2024-03-11 23:35:23 +0900

    Context분리하기

---
+++ 한입크기리액트스터디/section11/src/components/Editor.jsx
@@ -0,0 +1,43 @@
+import "./Editor.css";
+import { useState, useRef, useContext } from "react";
+import { TodoDispatchContext } from "../App";
+
+const Editor = () => {
+  const { onCreate } = useContext(TodoDispatchContext);
+  const contentRef = useRef();
+  const [content, setContent] = useState("");
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        onKeyDown={onKeydown}
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
+};
+
+export default Editor;
