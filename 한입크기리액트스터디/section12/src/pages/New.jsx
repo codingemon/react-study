7b2248263ddc7fbf9d26bbcb1c20dea6beddb6e3
snoopy1224 <misty12024@gmail.com>
2024-03-18 12:18:07 +0900

    수정하기 적용

--- 한입크기리액트스터디/section12/src/pages/New.jsx
+++ 한입크기리액트스터디/section12/src/pages/New.jsx
@@ -2,9 +2,16 @@ import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
-
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
   const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
 
   return (
     <div>
@@ -13,7 +20,7 @@ const New = () => {
         // nav -1 은 페이지를 뒤로 이동시켜준다
         leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
