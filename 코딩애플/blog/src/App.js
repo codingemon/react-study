e03f4a3b5c519f9582a71af52833e36d016741b3
snoopy1224 <misty12024@gmail.com>
2024-03-27 20:05:02 +0900

    modal

--- 코딩애플/blog/src/App.js
+++ 코딩애플/blog/src/App.js
@@ -10,32 +10,14 @@ function App() {
     "무슨무슨 맛집",
   ]);
   let [좋아요, set좋아요] = useState(0);
-
-  // const like = () => {
-  //   set좋아요(좋아요++);
-  // };
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>CodingEmon BLOG</h4>
       </div>
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy[0] = "여자코트 추천";
-          set글제목(copy);
-        }}
-      >
-        바뀌는 버튼
-      </button>
-      <button
-        onClick={() => {
-          set글제목([...글제목].sort());
-        }}
-      >
-        가나다순 정렬
-      </button>
+
       <div className="list">
         <h4>
           {글제목[0]}
@@ -55,11 +37,29 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={() => {
+            setModal(!modal);
+          }}
+        >
+          {글제목[2]}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {modal == true ? <Modal /> : null}
     </div>
   );
 }
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+};
 
 export default App;
