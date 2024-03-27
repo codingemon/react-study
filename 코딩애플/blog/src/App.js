166499d6339efb34d388049e6d31438ad6e4ad12
snoopy1224 <misty12024@gmail.com>
2024-03-28 00:46:20 +0900

    props

--- 코딩애플/blog/src/App.js
+++ 코딩애플/blog/src/App.js
@@ -9,55 +9,54 @@ function App() {
     "우동 맛집",
     "무슨무슨 맛집",
   ]);
-  let [좋아요, set좋아요] = useState(0);
+  let [좋아요, set좋아요] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [여자옷, set여자옷] = useState("여자코트 추천");
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>CodingEmon BLOG</h4>
       </div>
+      {글제목.map(function (a, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(!modal);
+              }}
+            >
+              {글제목[i]}
+              <span
+                onClick={() => {
+                  let copy = [...좋아요];
+                  copy[i] = copy[i] += 1;
+                  set좋아요(copy);
+                }}
+              >
+                👍
+              </span>
+              {좋아요[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
 
-      <div className="list">
-        <h4>
-          {글제목[0]}
-          <span
-            onClick={() => {
-              set좋아요((좋아요 += 1));
-            }}
-          >
-            👍
-          </span>
-          {좋아요}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4
-          onClick={() => {
-            setModal(!modal);
-          }}
-        >
-          {글제목[2]}
-        </h4>
-        <p>2월 17일 발행</p>
-      </div>
-
-      {modal == true ? <Modal /> : null}
+      {modal == true ? (
+        <Modal set글제목={set글제목} 글제목={글제목} 여자옷={여자옷} />
+      ) : null}
     </div>
   );
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글 수정</button>
     </div>
   );
 };
