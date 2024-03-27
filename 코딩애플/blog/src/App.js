bdf0196b7ffe6879a604e83aef11eb9ac64d65ce
snoopy1224 <misty12024@gmail.com>
2024-03-27 19:10:34 +0900

    array, object state

--- 코딩애플/blog/src/App.js
+++ 코딩애플/blog/src/App.js
@@ -1,18 +1,53 @@
+/* eslint-disable */
+
 import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
+  let [글제목, set글제목] = useState([
+    "남자코트 추천",
+    "우동 맛집",
+    "무슨무슨 맛집",
+  ]);
+  let [좋아요, set좋아요] = useState(0);
 
-  let [글제목, b] = useState(["남자코트 추천", "우동 맛집", "무슨무슨 맛집"]);
+  // const like = () => {
+  //   set좋아요(좋아요++);
+  // };
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>CodingEmon BLOG</h4>
       </div>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = "여자코트 추천";
+          set글제목(copy);
+        }}
+      >
+        바뀌는 버튼
+      </button>
+      <button
+        onClick={() => {
+          set글제목([...글제목].sort());
+        }}
+      >
+        가나다순 정렬
+      </button>
       <div className="list">
-        <h4>{글제목[0]}</h4>
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              set좋아요((좋아요 += 1));
+            }}
+          >
+            👍
+          </span>
+          {좋아요}
+        </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
