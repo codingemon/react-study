7811ba7ee1922d79d242dfb4bf68de2048052b91
snoopy1224 <misty12024@gmail.com>
2024-03-28 22:24:20 +0900

    upload : img

--- 코딩애플/shop/src/App.js
+++ 코딩애플/shop/src/App.js
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App() {
@@ -9,11 +9,19 @@ function App() {
           <Navbar.Brand href="#home">CodingEmonShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">EmonGoods</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">안녕</div>
+          <div className="col-md-4">안녕</div>
+          <div className="col-md-4">안녕</div>
+        </div>
+      </div>
     </div>
   );
 }
