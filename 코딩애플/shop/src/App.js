803a70c28ff3b0386c02746c1246bb1fd837c7ea
snoopy1224 <misty12024@gmail.com>
2024-03-29 10:56:29 +0900

    img

--- 코딩애플/shop/src/App.js
+++ 코딩애플/shop/src/App.js
@@ -17,9 +17,30 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">안녕</div>
-          <div className="col-md-4">안녕</div>
-          <div className="col-md-4">안녕</div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
         </div>
       </div>
     </div>
