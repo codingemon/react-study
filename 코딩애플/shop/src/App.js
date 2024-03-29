b3bdb0c0ce859248f25ee5967ab4ca272ea4ebf8
snoopy1224 <misty12024@gmail.com>
2024-03-29 11:48:54 +0900

    props

--- 코딩애플/shop/src/App.js
+++ 코딩애플/shop/src/App.js
@@ -22,9 +22,24 @@ function App() {
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <ShoesList />
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} i={i + 1} />;
+          })}
         </div>
       </div>
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img
+        src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"}
+        width="80%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.content}</p>
     </div>
   );
 }
--- 코딩애플/shop/src/ShoesList.js
+++ 코딩애플/shop/src/ShoesList.js
@@ -6,7 +6,10 @@ const ShoesList = () => {
   return shoes.map(function (a, i) {
     return (
       <div className="col-md-4">
-        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+        <img
+          src={"https://codingapple1.github.io/shop/shoes" + (i + 1) + ".jpg"}
+          width="80%"
+        />
         <h4>{shoes[i].title}</h4>
         <p>{shoes[i].content}</p>
       </div>
