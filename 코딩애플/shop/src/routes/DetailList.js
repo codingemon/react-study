d5210c314dbb2dd60b4dbfa097244ea3f58c5f10
snoopy1224 <misty12024@gmail.com>
2024-04-03 19:38:49 +0900

    detailList

--- 코딩애플/shop/src/routes/DetailList.js
+++ 코딩애플/shop/src/routes/DetailList.js
@@ -1,4 +1,11 @@
-const DetailList = () => {
+import { useParams } from "react-router-dom";
+
+const DetailList = (props) => {
+  let { id } = useParams();
+  let seachItem = props.shoes.find((x) => {
+    return x.id == id;
+  });
+
   return (
     <div className="container">
       <div className="row">
@@ -9,9 +16,9 @@ const DetailList = () => {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">상품명</h4>
-          <p>상품설명</p>
-          <p>120000원</p>
+          <h4 className="pt-5">{seachItem.title}</h4>
+          <p>{seachItem.content}</p>
+          <p>{seachItem.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
