684093855fcbdfb2d6ae47f995f4f78c23ff0cc3
snoopy1224 <misty12024@gmail.com>
2024-04-05 22:08:28 +0900

    useEffect

--- 코딩애플/shop/src/routes/DetailList.js
+++ 코딩애플/shop/src/routes/DetailList.js
@@ -1,6 +1,26 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+// let Btn = styled.button`
+//   background: ${(props) => props.bg};
+//   color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+//   padding: 10px;
+// `;
+
+// let BlackBox = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `;
 
 const DetailList = (props) => {
+  useEffect(() => {
+    console.log("안녕");
+  });
+
+  let [count, setCount] = useState(0);
+
   let { id } = useParams();
   let seachItem = props.shoes.find((x) => {
     return x.id == id;
@@ -8,6 +28,15 @@ const DetailList = (props) => {
 
   return (
     <div className="container">
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
+      {/* <Btn bg="blue">버튼</Btn> */}
+      {/* <Btn bg="yellow">버튼</Btn> */}
       <div className="row">
         <div className="col-md-6">
           <img
